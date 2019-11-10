ded775f7c7221ebcdbc241cfae93c8c69adc7185
Ahmed Bilal <ahbilal@microsoft.com>
2019-11-09 22:13:02 -0800

    qwe

--- application.py
+++ application.py
@@ -9,10 +9,9 @@ app = Flask(__name__)
 def makecalc():
     data = [[1, 1, 70, 1, 1, 100.25]]
     qwe=2+2
-    #listToStr = np.array2string(np.sum(np.array(data)))
+    listToStr = np.array2string(np.sum(np.array(data)))
     #listToStr="hello"
-    return "hello"
-    #return listToStr
+    return listToStr
     
 
     #return np.array2string(np.sum(np.array(data)))
