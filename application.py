8453a52d41447b8e15d01cea32f0eb2e2802b5fb
Ahmed Bilal <ahbilal@microsoft.com>
2019-11-09 19:24:54 -0800

    update

--- application.py
+++ application.py
@@ -1,6 +1,21 @@
-from flask import Flask
+from flask import Flask, request, redirect, url_for, flash, jsonify
+import numpy as np
+import pickle as p
+import json
+
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World. Just a check!"
+
+@app.route('/api/', methods=['POST'])
+def makecalc():
+    data = request.get_json()
+    prediction = np.array2string(model.predict(data))
+
+    return jsonify(prediction)
+
+if __name__ == '__main__':
+    modelfile = 'models/final_prediction.pickle'
+    model = p.load(open(modelfile, 'rb'))
+    app.run(debug=False, host='0.0.0.0')
+    
