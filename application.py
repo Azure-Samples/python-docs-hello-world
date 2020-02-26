23aeaa5b776f4027d01149818e19943a6d9fb0de
esteebie <36155806+esteebie@users.noreply.github.com>
2020-02-26 17:43:32 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,10 +1,12 @@
-#from flask import Flask
+from flask import Flask
 #import azure-ai-textanalytics
-#app = Flask(__name__)
+app = Flask(__name__)
 
-#@app.route("/")
-def hello():
-    return "running 2"
+@app.route("/")
+print("abc")
+
+#def hello():
+#    return "running 2"
 
 
 #from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
