a9e1e4d29278659460a2b805ea8f33de016cbcbd
esteebie <36155806+esteebie@users.noreply.github.com>
2020-02-27 09:51:54 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,8 +1,9 @@
 from flask import Flask
 from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
-#from azure.storage.blob import BlockBlobService, BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlockBlobService
+#, BlobServiceClient, BlobClient, ContainerClient
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "running 4"
+    return "running 5"
