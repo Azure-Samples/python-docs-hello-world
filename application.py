d7f695537c3ae3f7172f1c1b763f998ea27847d8
esteebie <36155806+esteebie@users.noreply.github.com>
2020-02-27 09:19:40 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,27 +1,7 @@
-print "Hello World"
-# This program adds two numbers
+from flask import Flask
+import azure-ai-textanalytics
+app = Flask(__name__)
 
-num1 = 1.5
-num2 = 6.3
-
-# Add two numbers
-sum = float(num1) + float(num2)
-
-# Display the sum
-print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
-
-#from flask import Flask
-#import azure-ai-textanalytics
-#app = Flask(__name__)
-
-#@app.route("/")
-#print("abc")
-
-#def hello():
-#    return "running 2"
-
-
-#from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
-#from azure.storage.blob import BlockBlobService, BlobServiceClient, BlobClient, ContainerClient
-#print(dir())
-#print("Hola")
+@app.route("/")
+def hello():
+    return "running 3"
