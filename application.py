3af59ce3ff651c1d3bdeb4664da421599130ffbc
esteebie <36155806+esteebie@users.noreply.github.com>
2020-02-27 09:44:45 +0000

    Update application.py

--- application.py
+++ application.py
@@ -1,6 +1,6 @@
 from flask import Flask
-from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
-from azure.storage.blob import BlockBlobService, BlobServiceClient, BlobClient, ContainerClient
+#from azure.ai.textanalytics import TextAnalyticsClient, TextAnalyticsApiKeyCredential
+#from azure.storage.blob import BlockBlobService, BlobServiceClient, BlobClient, ContainerClient
 app = Flask(__name__)
 
 @app.route("/")
