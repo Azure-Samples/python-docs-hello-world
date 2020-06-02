f6f4912b545d0d811faa06f7db5f5597f223e27d
rich viana <rviana@yerp.com>
2020-06-02 10:07:03 -0400

    added bottle

--- application.py
+++ application.py
@@ -1,6 +1,14 @@
-from flask import Flask
-app = Flask(__name__)
+import bottle
+from bottle import route, run, Response
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+
+app = bottle.default_app()
+
+@route('/')
+def index():
+    """Returns standard text response."""
+    return Response("my bottle app is up and running!")
+
+if __name__ == "__main__":
+    run(host="0.0.0.0", port=5000, debug=False, reloader=True)
+
