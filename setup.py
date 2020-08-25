1e0bea6caa73078b1f22e0dce79019f4027c942f
sundar-swetha <swsundar@microsoft.com>
2020-08-25 15:59:14 +0800

    Add a setup.py file to create a python distribution library that can be published to PyPI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(
+    name="python-docs-hello-world",
+    version="1.0.0",
+    packages=['.'],
+    author='merlion',
+    description='python docs hello world'
+)
