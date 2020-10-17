6faf324d5f30f76061ee0cb6e78eb7bd119d466e
Cyrus <cyjackso@microsoft.com>
2020-10-17 17:04:50 +0530

    Adds support for mysql

--- app.py
+++ app.py
@@ -1,6 +1,22 @@
 from flask import Flask
+import mysql.connector
 app = Flask(__name__)
+
+server = "dripper.mysql.database.azure.com"
+database = "greenhouse"
+username = "points"
+password = "Dripper#1"
+# MySQL configurations
+cnxn = mysql.connector.connect(host=server,database=database,username=username, password=password, autocommit=True)
+cursor = cnxn.cursor()
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    cursor.execute("SELECT * FROM stats")
+    myresult = cursor.fetchall()
+    result = ""
+    for row in myresult:
+        print(row)
+        result = result + str(row)
+
+    return result
