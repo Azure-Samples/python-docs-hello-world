e7144b7124c44a7ebec6e318c0b66661d7c2decb
x0x0b <42596409+x0x0b@users.noreply.github.com>
2021-04-28 21:53:24 +0900

    cosmos test

--- app.py
+++ app.py
@@ -1,6 +1,36 @@
+import os
+
+import azure.cosmos.cosmos_client as cosmos_client
+import azure.cosmos.exceptions as exceptions
+
 from flask import Flask
 app = Flask(__name__)
+
+HOST = os.environ.get('COSMOSDB_URI', 'localhost')
+MASTER_KEY = os.environ.get('SQLCONNSTR_RO_PRIMARY', 'XXX')
+
+client = cosmos_client.CosmosClient(HOST, {'masterKey': MASTER_KEY} )
+
+def list_databases(client):
+    print("\n4. List all Databases on an account")
+    print('Databases:')
+
+    databases = list(client.list_databases())
+
+    if not databases:
+        return
+
+    for database in databases:
+        return database['id']
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route("/list")
+def get_list():
+    try:
+        return list_databases(client)
+    except Exception as e:
+        return "error"
