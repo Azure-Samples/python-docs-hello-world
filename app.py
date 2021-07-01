4d123f3fe74eb329f914e98c8071c4dfccf4a343
Fernando <64105564+fxbloi@users.noreply.github.com>
2021-07-01 17:04:00 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,8 +4,3 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World 1234!"
-
-@app.route("/hola")
-def hola():
-    return "<h1 style="color:orange;">Hello, Deepdatas 1234!</h1>"
-    
