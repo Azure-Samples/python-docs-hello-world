32f5e1c3eb27d0dc9b8336621d128b7c06033dd0
Ivan Torres <trrs.ivan@gmail.com>
2021-07-05 15:16:29 -0300

    Se corrige funcion de login y se agrega render de index

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
     return "Hello, World 1234!"
 
-
 @app.route("/login")
 def login():
-    render_template('login.html')
+    return render_template('login.html')
+
+@app.route("/index")
+def index():
+    return render_template('index.html')
