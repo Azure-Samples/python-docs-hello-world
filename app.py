42e34db0c2da37c4ddc30ce52abccc2c2d8f1856
Fernando <64105564+fxbloi@users.noreply.github.com>
2021-09-22 12:12:44 -0300

    Update app.py

--- app.py
+++ app.py
@@ -19,9 +19,9 @@ db = SQLAlchemy(app)
 @app.route("/")
 
 
-@app.route("/index")
+@app.route("/login")
 def index():
-    return render_template('index.html')
+    return render_template('login.html')
 
 
 
