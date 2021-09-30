80ef8f504de2b04b7a2faa5b8b9a6b1e6a323c0c
Ivan Torres <trrs.ivan@gmail.com>
2021-09-30 08:40:33 -0300

    Se elimina lo que no se va a usar

--- Readme.txt
+++
@@ -1,6 +0,0 @@
-Thanks for downloading this template!
-
-Template Name: Arsha
-Template URL: https://bootstrapmade.com/arsha-free-bootstrap-html-template-corporate/
-Author: BootstrapMade.com
-License: https://bootstrapmade.com/license/
--- app.py
+++ app.py
@@ -3,38 +3,13 @@ from flask_bootstrap import Bootstrap
 from flask_sqlalchemy import SQLAlchemy
 import os
 
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-
 app = Flask(__name__)
-bootstrap = Bootstrap(app)
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///' + os.path.join(basedir, 'data.sqlite')
-app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
-#app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
-db = SQLAlchemy(app)
-
-
 
 @app.route("/")
-
 
 @app.route("/index")
 def index():
     return render_template('index.html')
 
-
-
-
-
-class User(db.Model):
-	__tablename__ = 'User'
-	id = db.Column(db.Integer, primary_key = True)
-	user = db.Column(db.String(64), unique=True)
-	password = db.Column(db.String(64))
-	azure_item_id = db.Column(db.String(64))
-
-	def __repr__(self):
-		return '<User %r>' % self.username
 
 
