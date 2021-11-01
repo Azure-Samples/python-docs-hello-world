ce988f670be08352f29b5bb5fb87d4a5770975a4
avishenhav76 <avi.shenhav76@gmail.com>
2021-11-01 18:08:21 +0200

    removed mysql

--- app.py
+++ app.py
@@ -1,28 +1,18 @@
 #from requests import request
 import json
 from flask import Flask, request,  jsonify
-import mysql.connector
 from datetime import datetime
-
-
-try:
-    x = 0
-    mydb = mysql.connector.connect(user="LocationAdmin1@locatormysqlserver", password="LovelyLocation1!",
-                                   host="locatormysqlserver.mysql.database.azure.com",  database="new_schema")
-    x = 1
-except Exception as ex:
-    print(ex)
 
 
 app = Flask(__name__)
 
 
-def InsertLocation(lot, lat, phoneid, dt):
-    mycursor = mydb.cursor()
-    sql = "INSERT INTO location (lot, lat,phoneid,dt) VALUES (%s, %s,%d,%d)"
-    val = (lot, lat, phoneid, dt)
-    mycursor.execute(sql, val)
-    mydb.commit()
+# def InsertLocation(lot, lat, phoneid, dt):
+#     mycursor = mydb.cursor()
+#     sql = "INSERT INTO location (lot, lat,phoneid,dt) VALUES (%s, %s,%d,%d)"
+#     val = (lot, lat, phoneid, dt)
+#     mycursor.execute(sql, val)
+#     mydb.commit()
 
 
 @app.route("/testconnection")
@@ -36,25 +26,25 @@ def hello():
     return "Hello, Azure1!"
 
 
-@app.route("/location", methods=['POST'])
-def Location():
-    try:
-        content = request.get_json()
-        lot = content["longitude"]
-        lat = content["latitude"]
-        sdt = content["datetime"]
-        phoneid = content["phoneid"]
-        dt = datetime.strptime(sdt, '%d/%m/%y %H:%M:%S')
-        InsertLocation(lot, lat, phoneid, dt)
+# @app.route("/location", methods=['POST'])
+# def Location():
+#     try:
+#         content = request.get_json()
+#         lot = content["longitude"]
+#         lat = content["latitude"]
+#         sdt = content["datetime"]
+#         phoneid = content["phoneid"]
+#         dt = datetime.strptime(sdt, '%d/%m/%y %H:%M:%S')
+#         InsertLocation(lot, lat, phoneid, dt)
 
-        data = json.dumps(content)
-        print(data)
-        # datetime = request.form.get('datetime')
-        # longitude = request.form.get('longitude')
-        # Latitude = request.form.get('latitude')
-        # phoneid = request.form.get('phoneid')
-    except Exception as ex:
-        print(ex)
+#         data = json.dumps(content)
+#         print(data)
+#         # datetime = request.form.get('datetime')
+#         # longitude = request.form.get('longitude')
+#         # Latitude = request.form.get('latitude')
+#         # phoneid = request.form.get('phoneid')
+#     except Exception as ex:
+#         print(ex)
 
 
 # app.run()
--- client.py
+++ client.py
@@ -1,11 +1,22 @@
 from datetime import datetime
 import mysql.connector
+import socket
+
+hostname = socket.gethostname()
+local_ip = socket.gethostbyname(hostname)
+
+print(local_ip)
+
 
 mydb = None
 try:
     x = 0
-    mydb = mysql.connector.connect(user="locationadmin@locatormysqlserver1", password="LovelyLocation1",
-                                   host="locatormysqlserver1.mysql.database.azure.com", database="sys")
+    #hostx = "52.128.23.153"
+    hostx = "locatormysqlserver.mysql.database.azure.com"
+    userx = "LocationAdmin1@locatormysqlserver"
+
+    mydb = mysql.connector.connect(user=userx, password="LovelyLocation1!",
+                                   host=hostx, port=3306)
     print(mydb)
     x = 1
 except Exception as ex:
