b79ae09453b523411acc2b616ff005eaa33ceee9
taminta <50441508+taminta@users.noreply.github.com>
2021-12-13 22:23:10 +0900

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ def add_header(r):
     Add headers to both force latest IE rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
     """
-    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Cache-Control"] = "no-store"
     r.headers["Pragma"] = "no-cache"
     r.headers["Expires"] = "0"
-    r.headers['Cache-Control'] = 'public, max-age=0'
+    r.headers['CheckHeader'] = 'Check'
     return r
