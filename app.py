430dd89662eecadeecc8584cb8193f1bc825b1f7
taminta <50441508+taminta@users.noreply.github.com>
2021-12-13 22:18:01 +0900

    add no cache config

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ app = Flask(__name__)
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.after_request
+def add_header(r):
+    """
+    Add headers to both force latest IE rendering engine or Chrome Frame,
+    and also to cache the rendered page for 10 minutes.
+    """
+    r.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    r.headers["Pragma"] = "no-cache"
+    r.headers["Expires"] = "0"
+    r.headers['Cache-Control'] = 'public, max-age=0'
+    return r
